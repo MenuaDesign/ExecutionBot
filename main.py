845380bf1681499275f85265f45aec57dc494e09
MenuaDesign <67699503+MenuaDesign@users.noreply.github.com>
2021-02-04 17:14:10 +0100

    1.0.5

--- main.py
+++ main.py
@@ -3,12 +3,8 @@ import os
 from discord.utils import get
 import time
 from discord.ext import commands
-
 BOT_PREFIX ='-'
-
 client = commands.Bot(command_prefix=BOT_PREFIX)
-
-
 Executioner = (246652133464735746)
 VoteCount = 0
 Prisoner = None
@@ -22,21 +18,16 @@ role = None
 roles = ["Member","Co-Owner","Moderator", "OG", "Owner", "Admin", "WARZONE/COD","Hospital"]
 lastwords = False
 execute = False
-
-
+first = True
 @client.event
 async def on_ready():
     print("Logged {0.user}\nReady".format(client))
-
 @client.command()
 async def addrole(ctx,role:discord.Role, user: discord.Member):
     await user.add_roles(role)
-
 @client.command()
 async def removerole(ctx,role:discord.Role, user: discord.Member):
     await user.remove_roles(role)
-
-
 @client.event
 async def on_message(message):
     try:
@@ -53,6 +44,7 @@ async def on_message(message):
             global roles
             global lastwords
             global execute
+            global first
 
             if message.author.id == Executioner:
                 if message.content[0] == "-":
@@ -86,7 +78,15 @@ async def on_message(message):
                             await message.channel.send(f"```We shall kill {usr} [TYPE 'kill' to vote]```")
                             Vote = True
                         return
-            if Vote:
+            if lastwords and message.author.id != Prisoner:
+                await message.delete()
+                return
+            if lastwords and message.author.id == Prisoner:
+                print(lastwords)
+                print(message.author.id)
+                print(Prisoner)
+                execute = True
+            elif Vote:
                 if message.content == "abord" and message.author.id == Executioner:
                     Vote = False
                     VoteCount = 0
@@ -97,6 +97,7 @@ async def on_message(message):
                     return
                 elif message.content == "kill":
                     AllVotes.append(message.author.id)
+                    global t
                     t = 0
                     for i in AllVotes:
                         if i not in Seen:
@@ -105,57 +106,65 @@ async def on_message(message):
                             VotedList.append(i)
                             Seen.add(i)
                             if VoteCount == 5 or VoteCount == 10:
-                                await message.channel.send("```" + "~~" + ("|" * VoteCount) + "~~" + "```")
+                                await message.channel.send("```" + "~~" + ("|" * (VoteCount-1)) + "~~" + "```")
                             else:
                                 await message.channel.send("```"+"|"*VoteCount+"```")
 
-                            if VoteCount == 1:
-                                await message.channel.send(f"The execution will be executed, what are your last words {usr}")
-                                lastwords = True
-                                if not lastwords and message.author.id != Prisoner:
-                                    await message.delete()
+                            if VoteCount == 5:
+                                if first:
+                                    await message.channel.send(f"```The execution will be executed, what are your last words {usr}```")
+                                    lastwords = True
+                                    first = False
                                     return
-                                else:
-                                    execute = True
-                            if execute:
-                                await message.channel.send("The executioner is sharpening his blade")
-                                # Image sharpening
-                                time.sleep(3)
-                                # Image of executer runing
-                                time.sleep(3)
-                                #image of prisoner tied up
-                                time.sleep(3)
-                                #image of beheading
-                                member = user
-                                mp = "-addrole Beheaded " + str(member)
-                                message.content = mp
-                                await client.process_commands(message)
-                                for h in roles:
-                                    mp = "-removerole " + h + " " + str(member)
-                                    message.content = mp
-                                    await client.process_commands(message)
-                                await message.channel.send(f"```{usr} has been beheaded by the executer.```")
-                                time.sleep(3)
-                                #image of beheaded
-                                Vote = False
-                                VoteCount = 0
-                                AllVotes = []
-                                Seen = set()
-                                VotedList = []
-                                time.sleep(5)
-                                mp = "-removerole Beheaded " + str(member)
-                                message.content = mp
-                                await client.process_commands(message)
-                                mp = "-addrole Hospital " + str(member)
-                                message.content = mp
-                                await client.process_commands(message)
-                                await message.channel.send(f"```{usr} has been healed in the hospital.```")
-                                return
                     if t == 0:
                         await message.delete()
+
                 else:
                     await message.delete()
                     return
+
+            if execute:
+                await message.channel.send("```The executioner is sharpening his blade```")
+                await message.channel.send(file=discord.File('img/Sharping.jpg'))
+                time.sleep(3)
+                await message.channel.send("```The executioner is heading towards the execution table```")
+                await message.channel.send(file=discord.File('img/Running.jpg'))
+                time.sleep(3)
+                await message.channel.send("```The prisoner is tied up on the table```")
+                await message.channel.send(file=discord.File('img/Waiting.gif'))
+                time.sleep(3)
+                await message.channel.send("```The executioner is swinging his blade```")
+                await message.channel.send(file=discord.File('img/Beheading.jpg'))
+                time.sleep(1)
+                await message.channel.send(f"```{usr} has been beheaded by the executioner.```")
+                member = user
+                mp = "-addrole Beheaded " + str(member)
+                message.content = mp
+                await client.process_commands(message)
+                for h in roles:
+                    mp = "-removerole " + h + " " + str(member)
+                    message.content = mp
+                    await client.process_commands(message)
+                time.sleep(3)
+                await message.channel.send(file=discord.File('img/Killed.jpg'))
+                Vote = False
+                VoteCount = 0
+                AllVotes = []
+                Seen = set()
+                VotedList = []
+                lastwords = False
+                execute = False
+                first = True
+                await message.channel.send("```The execution is over.```")
+                time.sleep(5)
+                mp = "-removerole Beheaded " + str(member)
+                message.content = mp
+                await client.process_commands(message)
+                mp = "-addrole Hospital " + str(member)
+                message.content = mp
+                await client.process_commands(message)
+                await message.channel.send(f"```{usr} has been healed in the hospital.```")
+                return
             else:
                 if message.content == "kill":
                     await message.channel.send("```No one is being executed```")
@@ -166,4 +175,4 @@ async def on_message(message):
     except ValueError:
         await message.channel.send("```Don't waste my time!```")
 
-client.run(os.environ['TOKEN'])
+client.run('ODA2NTUyMTM4NTIzMjEzODc0.YBrGNg.0MHZAEOGx5-wh3_HTcRkAzycCGM')
